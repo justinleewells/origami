8e5130a80ab5e939be1a9f040029167bb3f3bc39
Justin L. Wells <justinleewells@icloud.com>
2025-06-29 11:28:58 -0500

    Kill both processes when killing test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+pgid=$(ps -o pgid= $$ | tr -d ' ')
+trap 'kill -- -$pgid' 0
+
 ls ./modules/**/src/*.{cpp,h} | entr -cp make $1_test &
 ls ./bin/$1_test | entr -cp ctest --output-on-failure ./bin/$1_test
