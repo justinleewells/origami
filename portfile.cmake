6db56430e04d8cdce64f19f4281c6134e18fc595
Justin L. Wells <justinleewells@icloud.com>
2025-06-29 12:39:33 -0500

    First vcpkg attempt

---
+++ portfile.cmake
@@ -0,0 +1,20 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO justinleewells/origami
+  REF "${VERSION}"
+  SHA512 0
+  HEAD_REF cmake_sample_lib
+)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "origami")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
